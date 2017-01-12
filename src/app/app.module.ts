4a067e639d74aeab35620d2bfa45b96eaa3283a9
joandvgv <joandv.gil@gmail.com>
2017-01-12 12:21:43 -0400

    Added some console.logs before ngmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { routing } from './app.router';
 import { effects, store, instrumentation } from './store';
 import { SharedModule } from './shared/shared.module';
+
+
+
+
+
+ console.log(AppComponent);
+ console.log(routing);
+ console.log(effects, store, instrumentation);
+ console.log(SharedModule);
+ console.log(HttpModule);
+ console.log(BrowserModule);
+ console.log(FormsModule);
+
+
+
 
 @NgModule({
   declarations: [
