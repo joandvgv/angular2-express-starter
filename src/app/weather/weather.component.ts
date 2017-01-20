b380d8855a2ed51ec39f2c70a4b339722020fb19
joandvgv <joandv.gil@gmail.com>
2017-01-20 13:29:56 -0400

    added form response

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -25,31 +25,14 @@ export class WeatherComponent implements OnInit {
 
     $(document).ready(function()
 {
-  $('#load-more-content').click(function()
-  {
-    
-    $('#more-content').toggleClass('shown');
+ 
+      $('form').live('submit', function(){
+      $.post($(this).attr('action'), $(this).serialize(), function(response){
+           alert("We'll be in touch soon");
+      },'application/x-form-urlencoded');
+      return false;
+   });
 
-    $('#load-more-content').hide();
-
-    if( $('#more-content').hasClass('shown') )
-    {
-      $('#load-more-content').text('Hide content');
-      $('#more-content').fadeIn('slow', function()
-      {
-        $('#load-more-content').fadeIn('slow');
-      });
-    }
-    else
-    {
-      $('#load-more-content').text('Load some content');
-      $('#more-content').fadeOut('slow', function()
-      {
-        $('#load-more-content').fadeIn('slow');
-      });
-    }
-  });
-});
 
     this.form = this.fb.group({
       'latitude': ['43.815623', Validators.required],
