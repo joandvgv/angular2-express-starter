c1ef05521dad3dc302a35aa9bc2b988b04fe2835
joandvgv <joandv.gil@gmail.com>
2017-01-20 16:59:16 +0100

    added contact form and edited routes

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,10 +2,10 @@ import { RouterModule, Route } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 const routes: Route[] = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard'},
-  { loadChildren: 'app/dashboard/dashboard.module#DashboardModule', path: 'dashboard' },
-  { loadChildren: 'app/profile/profile.module#ProfileModule', path: 'profile' },
-  { loadChildren: 'app/weather/weather.module#WeatherModule', path: 'weather' }
+  { path: '', pathMatch: 'full', redirectTo: 'inicio'},
+  { loadChildren: 'app/dashboard/dashboard.module#DashboardModule', path: 'inicio' },
+  { loadChildren: 'app/profile/profile.module#ProfileModule', path: 'galeria' },
+  { loadChildren: 'app/weather/weather.module#WeatherModule', path: 'contacto' }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
@@ -7,10 +7,12 @@ import { IAppState } from '../store/index';
 import { WEATHER_GET, SELECT_CITY } from '../store/weather/weather.actions';
 import { IWeather } from '../store/weather/weather.reducer';
 
+declare var $: any;
 @Component({
   selector: 'app-weather',
   styleUrls: ['./weather.component.css'],
-  templateUrl: './weather.component.html'
+  templateUrl: './weather.component.html',
+  encapsulation: ViewEncapsulation.None
 })
 export class WeatherComponent implements OnInit {
   form: FormGroup;
@@ -20,6 +22,34 @@ export class WeatherComponent implements OnInit {
   constructor(public fb: FormBuilder, public store: Store<IAppState>) {}
 
   ngOnInit(): void {
+
+    $(document).ready(function()
+{
+  $('#load-more-content').click(function()
+  {
+    
+    $('#more-content').toggleClass('shown');
+
+    $('#load-more-content').hide();
+
+    if( $('#more-content').hasClass('shown') )
+    {
+      $('#load-more-content').text('Hide content');
+      $('#more-content').fadeIn('slow', function()
+      {
+        $('#load-more-content').fadeIn('slow');
+      });
+    }
+    else
+    {
+      $('#load-more-content').text('Load some content');
+      $('#more-content').fadeOut('slow', function()
+      {
+        $('#load-more-content').fadeIn('slow');
+      });
+    }
+  });
+});
 
     this.form = this.fb.group({
       'latitude': ['43.815623', Validators.required],
