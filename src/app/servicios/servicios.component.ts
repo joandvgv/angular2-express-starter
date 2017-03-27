216f16f83697382e491592fe099ecfe334f1431e
joandvgv <joandv.gil@gmail.com>
2017-03-27 15:12:26 -0400

    modified nav bar

---
+++ src/app/servicios/servicios.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'servicios',
+    templateUrl: 'servicios.component.html',
+    styleUrls: ['servicios.component.css']
+})
+export class ComponentNameComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
